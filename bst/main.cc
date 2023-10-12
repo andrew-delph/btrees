41e5688c6534fa1230ffb44952768b892617ca72
Andrew Delph <andrewdelph@live.ca>
2023-10-12 16:15:04 -0400

    time lapse bst main

--- bst/main.cc
+++ bst/main.cc
@@ -1,10 +1,36 @@
 #include <iostream>
 using namespace std;
 #include "bst.h"
+#include <iostream>
+#include <string>
+#include <random>
+
+#include <iostream>
+#include <chrono>
+
+string generateRandomString(int x)
+{
+    static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(0, sizeof(alphanum) - 2);
+
+    string result;
+    for (int i = 0; i < x; ++i)
+    {
+        result += alphanum[dis(gen)];
+    }
+
+    return result;
+}
 
 int main()
 {
-
+    auto start = std::chrono::high_resolution_clock::now();
     Item *item = new Item;
     item->key = "zz";
     item->value = "xx";
@@ -24,4 +50,18 @@ int main()
     cout << "blank: " << tree.get("???") << std::endl;
 
     cout << "4 " << tree.get("test4") << std::endl;
+
+    for (int i = 0; i < 100000; ++i)
+    {
+        string key = generateRandomString(5);
+        string value = generateRandomString(5);
+        tree.put(key, value);
+    }
+
+    auto finish = std::chrono::high_resolution_clock::now();
+
+    // Calculate the difference between start and finish
+    std::chrono::duration<double> elapsed = finish - start;
+
+    std::cout << "Elapsed time: " << elapsed.count() << " s\n";
 }
