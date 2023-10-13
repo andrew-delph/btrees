baad34d45042668f28900d889ad039a8e0b29a4c
Andrew Delph <andrewdelph@live.ca>
2023-10-13 09:34:32 -0400

    c header example

--- hello.c
+++ hello.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+#include "test.h"
+#include "addition.h"
 int main()
 {
     printf("Hello, World!\n");
+    int result = test();
+    printf("%d\n", result);
     return 0;
 }
