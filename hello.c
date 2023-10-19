5b5bd963ecc8debf527e3a7a6bed27219e7de56e
Andrew Delph <andrewdelph@live.ca>
2023-10-19 09:32:59 -0400

    updat3e hhello test

--- hello.c
+++ hello.c
@@ -1,10 +1,43 @@
 #include <stdio.h>
 #include "test.h"
 #include "addition.h"
+
+struct Person
+{
+    char *name;
+};
+
+void change_name(struct Person *p)
+{
+    p->name = "Andrew";
+}
+
+void change_str(char str[])
+{
+    printf("c %c\n", str[2]);
+    // str[3] = 'X';
+    // char c = 'X';
+    // str[2] = &c;
+}
 int main()
 {
-    printf("Hello, World!\n");
-    int result = test();
-    printf("%d\n", result);
+    struct Person p0;
+    struct Person *p1 = &p0;
+    p1->name = "andrew";
+    change_name(p1);
+    printf("p1 = %s\n", p1->name);
+
+    char *str = "ANDREW";
+
+    change_str(str);
+
+    printf("str %s\n", str);
+    void test();
+    test();
     return 0;
+}
+
+void test()
+{
+    printf("TESTING!\n");
 }
