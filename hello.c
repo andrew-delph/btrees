6899be466451838c5250e844df5f6d9786b19424
Andrew Delph <andrewdelph@live.ca>
2023-10-19 10:53:57 -0400

    update hello.c

--- hello.c
+++ hello.c
@@ -19,25 +19,62 @@ void change_str(char str[])
     // char c = 'X';
     // str[2] = &c;
 }
+// int main()
+// {
+//     struct Person p0;
+//     struct Person *p1 = &p0;
+//     p1->name = "andrew";
+//     change_name(p1);
+//     printf("p1 = %s\n", p1->name);
+
+//     char *str = "ANDREW";
+
+//     change_str(str);
+
+//     printf("str %s\n", str);
+//     void test();
+//     test();
+//     return 0;
+// }
+
+// int main()
+// {
+//     printf("starting\n");
+//     register int total = 0;
+//     // int total = 0;
+//     int num = 100000000;
+//     for (int j = 0; j < 5; j++)
+//     {
+//         for (int i = 0; i < num; i++)
+//         {
+//             total++;
+//         }
+//         for (int i = 0; i < num; i++)
+//         {
+//             total--;
+//         }
+//         for (int i = 0; i < num; i++)
+//         {
+//             total++;
+//         }
+//     }
+
+//     printf("total = %d\n", total);
+//     return 0;
+// }
+void test(int i)
+{
+    if (i > 10)
+    {
+        return;
+    }
+    test(i + 1);
+}
+#define max(A, B) ((A) > (B) ? (A) : (B))
 int main()
 {
-    struct Person p0;
-    struct Person *p1 = &p0;
-    p1->name = "andrew";
-    change_name(p1);
-    printf("p1 = %s\n", p1->name);
-
-    char *str = "ANDREW";
-
-    change_str(str);
-
-    printf("str %s\n", str);
-    void test();
-    test();
+    printf("starting test().z\n");
+    test(0);
+    printf("max= %d\n", max('a', 2));
     return 0;
-}
-
-void test()
-{
-    printf("TESTING!\n");
 }
